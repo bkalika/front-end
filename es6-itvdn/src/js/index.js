d3bb9dfd6570b0020b02275a888a789f6a48560f
bkalika <bogdan.kalika@gmail.com>
2020-10-10 15:12:14 +0300

    first commit

---
+++ es6-itvdn/src/js/index.js
@@ -0,0 +1,7 @@
+import calculator from './calculator';
+
+var a = 5;
+var b = 61;
+
+var sum = calculator.sum(a, b);
+console.log(sum);
---
+++ front_end_learning/front_end_learning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace front_end_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
